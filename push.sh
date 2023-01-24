4aa0dd218c51c79f62481ee6038f2dd3e85972ff
abdularshad06 <abdularshad390@gmail.com>
2023-01-24 17:06:16 +0530

    Add existing project files to Git

---
+++ push.sh
@@ -0,0 +1,7 @@
+cd /home/admin/bash_script
+git init
+git add .
+git commit -m "Add existing project files to Git"
+git remote add origin https://github.com/abdularshad06/bash_script.git
+git push -u -f origin main
+
---
+++ system_defined_variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# system defined variables
+
+echo ${SHELL}
