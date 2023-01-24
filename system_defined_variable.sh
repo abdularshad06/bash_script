3255b3713a03ad92cf485aa9a87688d54931045d
abdularshad06 <abdularshad390@gmail.com>
2023-01-24 18:49:57 +0530

    Add existing project files to Git

--- system_defined_variable.sh
+++ system_defined_variable.sh
@@ -3,3 +3,23 @@
 # system defined variables
 
 echo ${SHELL}
+echo ${USER}
+echo ${HOME}
+echo ${OSTYPE}
+echo ${PATH}
+echo ${$}  # print process ID
+echo ${PPID} # print parant process ID 
+#sleep 60
+echo ${PWD}
+echo ${HOSTNAME}
+
+echo ${UID}
+#UID=10000
+#echo ${1000}
+#PATH=/bin/bash
+#echo $PATH
+
+echo ${BASH_VERSION}
+
+sleep 5
+echo ${SECONDS}
