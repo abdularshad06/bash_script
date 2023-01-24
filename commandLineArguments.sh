97cd1947040139a18a526885607f21987e13353d
abdularshad06 <abdularshad390@gmail.com>
2023-01-24 20:22:47 +0530

    Add existing project files to Git

--- commandLineArguments.sh
+++ commandLineArguments.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-name="arshad"
-age="30"
+# name="arshad"
+# age="30"
 
-#echo ${0}
-#echo ${1}
-#echo ${2}
+# #echo ${0}
+# #echo ${1}
+# #echo ${2}
 
-name=${1}
-age=${2}
-echo "My Name is ${name}, and my age is ${age}"
+# name=${1}
+# age=${2}
+# echo "My Name is ${name}, and my age is ${age}"
+
+# =================================================
+# echo $1
+# echo $2
+# echo $3
+# echo $4
+# echo $5
+# echo $6
+# echo $7
+# echo $8
+# echo $9
+# echo $10
+# echo $11
+# echo $12
+# echo $13
+# =================================================
+echo ${1}
+echo ${2}
+echo ${3}
+echo ${4}
+echo ${5}
+echo ${6}
+echo ${7}
+echo ${8}
+echo ${9}
+echo ${10}
+echo ${11}
+echo ${12}
+echo ${13}
