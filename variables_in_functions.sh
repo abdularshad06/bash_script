d9de26afee12545b512f554d318ba0f8ba86a8c1
abdularshad06 <abdularshad390@gmail.com>
2023-01-25 16:28:45 +0530

    Add existing project files to Git

---
+++ variables_in_functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+packageName="nginx"
+function install() { 
+local myname="arshad"
+echo "Installing ${1}"
+}
+
+function configuration() {
+packageName="tomcat"
+echo "tomcat"
+echo "Configuring ${1}"
+}
+
+echo "First ${packageName}"
+echo "myname = ${myname}"
+install "${packageName}"
+echo "myname = ${myname}"
+echo "Second ${packageName}"
+configuration "${packageName}"
+echo "Third ${packageName}"
