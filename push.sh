a6d2b648abfb4f6938bba42ab7ea8f3fdb090e8d
abdularshad06 <abdularshad390@gmail.com>
2023-01-25 22:54:33 +0530

    Add existing project files to Git

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@ cd /home/admin/bash_script
 git init
 git add .
 git commit -m "Add existing project files to Git"
-git remote add origin https://github.com/abdularshad06/bash_script.git
+#git remote add origin https://github.com/abdularshad06/bash_script.git
+git remote set-url origin https://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@github.com/abdularshad06/bash_script.git
 git push -u -f origin main
 
 # git config --list --show-origin
