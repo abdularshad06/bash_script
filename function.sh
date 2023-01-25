aeec4161e2f5cf69a5cd75d395c5a332ef7374f9
abdularshad06 <abdularshad390@gmail.com>
2023-01-25 14:08:42 +0530

    Add existing project files to Git

---
+++ function.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Syntax :
+
+function install() { # this method is very correct method
+# Installation Code.
+echo "Installation Code1"
+echo "Installation Code2"
+echo "Installation Code3"
+echo "Installation Code4"
+}
+
+configuration() {
+# configuration Code.
+echo "Configuration Code1"
+echo "Configuration Code2"
+echo "Configuration Code3"
+}
+
+function deployment {
+# Deployment Code.
+echo "Deployment Code1"
+echo "Deployment Code2"
+echo "Deployment Code3"
+}
+
+
+# function call.
+install
+configuration
+deployment
+
