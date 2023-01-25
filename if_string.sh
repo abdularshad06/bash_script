9e0e6055497931caba1b2bde12a28d635cd0bd60
abdularshad06 <abdularshad390@gmail.com>
2023-01-25 21:10:49 +0530

    Add existing project files to Git

--- if_string.sh
+++ if_string.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-name="abdul arshad"
+name="arshad abdul"
 othername="abdul arshad"
 
 # strongly recommended use [[]] double angular bracket with if
+
+# -n value of string is non zero
+if [[ -n ${name} ]]
+then
+    echo "Lenght of string is non zero"
+fi
+
+# -z value of string is zero
+if [[ -z ${name} ]]
+then
+    echo "Lenght of string is zero"
+fi
+
+# check both strings are equal
 if [[ ${name} == ${othername} ]]
 then
     echo "Both Strings are Equal"
 fi
 
+# check both strings are not equal
+if [[ ${name} != ${othername} ]]
+then
+    echo "Both Strings are not equal"
+fi
