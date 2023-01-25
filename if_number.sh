0e4f1b853a8ef6e40407987e036e71cd5320222c
abdularshad06 <abdularshad390@gmail.com>
2023-01-25 18:28:54 +0530

    Add existing project files to Git

---
+++ if_number.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+number=15
+
+if test $number -eq 5
+then
+    echo "Number is eq 5"
+fi
+
+# test alias -->  []
+if [ $number -eq 5 ]
+then
+    echo "Number is eq 5"
+fi
+
+if [ $number -lt 10 ]
+then
+    echo "Number is less then 10"
+fi
+
+if [ $number -gt 4 ]
+then
+    echo "Number is Grater then 4"
+fi
+
+if [ $number -ge 4 ]
+then
+    echo "Number is Grater or Eq 4"
+fi
+
+if [ $number -le 4 ]
+then
+    echo "Number is Less or Eq  4"
+fi
+
+if [ $number -ne 4 ]
+then
+    echo "Number is not eq 4"
+fi
