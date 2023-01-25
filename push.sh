faf4b62111692a2069dc2a5a29b6affb9996877f
abdularshad06 <abdularshad390@gmail.com>
2023-01-25 23:27:29 +0530

    Add existing project files to Git

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 tokenvar=${1}
 function token(){
   echo "$FUNCNAME" - STARTED
-  #echo "${tokenvar}"
   echo "git remote set-url origin https://${tokenvar}@github.com/abdularshad06/bash_script.git"
   echo "$FUNCNAME" - END
 }
@@ -11,7 +10,7 @@ git init
 git add .
 git commit -m "Add existing project files to Git"
 #git remote add origin https://github.com/abdularshad06/bash_script.git
-#git remote set-url origin https://github.com/abdularshad06/bash_script.git
+git remote set-url origin https://github.com/abdularshad06/bash_script.git
 token
 git push -u -f origin main
 
