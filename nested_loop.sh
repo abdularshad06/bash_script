6223efd2ea54837c51ffa148d089ee43c48ccaaf
abdularshad06 <abdularshad390@gmail.com>
2023-01-27 16:55:11 +0530

    Add existing project files to Git

--- nested_loop.sh
+++ nested_loop.sh
@@ -17,32 +17,32 @@
 
 # =======================================
 
-# initNumber=1
-# while [[ ${initNumber} -lt 3 ]]
-# do
-#     for i in item1 item2 item3
-#     do
-#         echo ${initNumber} - ${i}
-#         if [[ ${i} == item2 ]]
-#         then
-#             break 2;
-#         fi
-#     done    
-#     ((initNumber++))
-# done
-
-# =======================================
-
 initNumber=1
 while [[ ${initNumber} -lt 3 ]]
 do
     for i in item1 item2 item3
     do
+        echo ${initNumber} - ${i}
         if [[ ${i} == item2 ]]
         then
-            continue;
+            break 2;
         fi
-    echo ${initNumber} - ${i}
     done    
     ((initNumber++))
 done
+
+# =======================================
+
+# initNumber=1
+# while [[ ${initNumber} -lt 3 ]]
+# do
+#     for i in item1 item2 item3
+#     do
+#         if [[ ${i} == item2 ]]
+#         then
+#             continue;
+#         fi
+#     echo ${initNumber} - ${i}
+#     done    
+#     ((initNumber++))
+# done
