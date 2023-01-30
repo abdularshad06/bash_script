fae05e548195e61581294ea9c4858c2bd2b8a935
vagrant <vagrant@localhost.localdomain>
2023-01-30 10:44:00 +0000

    Add existing project files to Git

--- left_rightangle_number.sh
+++ left_rightangle_number.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# * 
-# * *
-# * * *
-# * * * *
-# * * * * *
+# 1 
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
 
 # for ((row=1; row<=5; row++))
 # do
 #     for ((col=1; col<=row; col++))
 #     do
-#         echo -n "* "
+#         echo -n "$col "
 #     done
 #     echo
 # done
 
-read -p "Enter Number in between 1-9: " number
+read -p "Enter Number between 1-9 : " number
 for ((row=1; row<=$number; row++))
 do
     for ((col=1; col<=row; col++))
     do
-        echo -n "* "
+        echo -n "$col "
     done
     echo
 done
--- right_rightangle.sh
+++ right_rightangle.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-
-# * 
-# * *
-# * * *
+       
+# * * * * * 
 # * * * *
-# * * * * *
-
+# * * *
+# * *
+# *
+              
 # for ((row=1; row<=5; row++))
 # do
-#     for ((col=1; col<=row; col++))
+#     for ((col=5; col>=row; col--))
 #     do
 #         echo -n "* "
 #     done
 #     echo
 # done
 
-read -p "Enter Number in between 1-9: " number
+read -p "Enter Number in between 1-9 : " number
 for ((row=1; row<=$number; row++))
 do
-    for ((col=1; col<=row; col++))
+    for ((col=$number; col>=row; col--))
     do
         echo -n "* "
     done
